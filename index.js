2c37f5dacb789b9750621b0497050ec73070a2de
Rohail Rafique <rohailrafique@gmail.com>
2021-09-21 22:29:31 -0400

    commit tree 0ceb4a66a35cf997fcd0dd50b39c7ec7d142e7f5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+
+
+const mondayWork = function(task = "go to the office") {
+return `This Monday, I will ${task}.`
+}
+
+mondayWork();
